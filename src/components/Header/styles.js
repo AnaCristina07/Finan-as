9f53326f8f013f4840f1a68e7892d579304e6296
Ana Cristina <cristinaengller@gmail.com>
2022-11-20 22:52:48 -0300

    Add files via upload

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: #25502d;
+  box-shadow: 0px 0px 15px rgba(0,0,0,0.4) 
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
